ce6c6445aff65e147257254609e46a00abff7cdf
Rana Soyak <rselvasoyak@gmail.com>
2023-01-30 00:06:38 -0500

    added the dropdown menu into app.js

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 // Dropdown Hamburger Menu 
 
+    // Namespacing for dropdown Menu
+    const dropMenu = {};
+
     // User clicks on the hamburger menu icon
         // Select the hamburger menu icon
-    const hamburgerMenu = document.querySelector('.hamburgerMenu');
-    const navLinks = document.querySelector('.navLinks');
+    dropMenu.hamburgerMenu = document.querySelector('.hamburgerMenu');
+    dropMenu.navLinks = document.querySelector('.navLinks');
     
     // on click - Menu items dopdown on the page.
-   hamburgerMenu.addEventListener('click', function() {
-    if(navLinks.classList.contains('showNav')) {
-        navLinks.classList.remove('showNav');
-        navLinks.classList.add('closing');
-    } else {
-        navLinks.classList.remove('closing');
-        navLinks.classList.add('showNav');
+    dropMenu.swipeProjects = () => {
+        dropMenu.hamburgerMenu.addEventListener('click', function() {
+         if(dropMenu.navLinks.classList.contains('showNav')) {
+             dropMenu.navLinks.classList.remove('showNav');
+             dropMenu.navLinks.classList.add('closing');
+         } else {
+             dropMenu.navLinks.classList.remove('closing');
+             dropMenu.navLinks.classList.add('showNav');
+         }
+     });
     }
-});
 
-// Projects Carousel Section
+    // On Page Load 
+    dropMenu.init = () => {
+        dropMenu.swipeProjects();
+    }
+    
+    dropMenu.init();
+    
+    
 
-// Scroll Loading Effect 
 
-// Skills Section Hover State - rest of icons turning black and white
+
+
+
+
+
+
+
+
