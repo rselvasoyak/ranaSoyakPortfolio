278eec2df3eb0a315eeddd09bc13bb1f2ae941b9
Rana Soyak <rselvasoyak@gmail.com>
2023-01-30 00:35:45 -0500

    started carousel functionality

--- app.js
+++ app.js
@@ -28,7 +28,41 @@
     
     dropMenu.init();
     
-    
+    // Projects Carousel Section
+
+    // NameSpace for Carousel 
+
+    const carousel = {};
+    // User opens the page and the first layout they see is the first project display
+    // User clicks right or left button to switch between project displays
+        // Get the necessary elements 
+            // Projects
+            carousel.projectsAll = document.querySelector(".carouselPhotos");
+            carousel.project = document.querySelector(".project")
+            // Buttons
+            carousel.prevBtn = document.querySelector(".left");
+            carousel.nextBtn = document.querySelector(".right");
+
+        // Event listener on the right and left buttons
+        carousel.swipeRight = () => {
+            nextBtn.addEventListener("click", (event) => {
+                carousel.slideWidth = project.clientWidth;
+                carousel.projectsAll.scrollRight += slideWidth;
+            });
+        };
+
+        carousel.swipeLeft = () => {
+            prevBtn.addEventListener("click", (event) => {
+            carousel.slideWidth = project.clientWidth;
+            projectsAll.scrollLeft -= slideWidth;
+            });
+        };
+
+        // Each click the content for image, the project number display and the links related to it will renew. 
+    // The user does not have to click oppesite direction button, the carousel will reset its order when the project amount is done. 
+// Scroll Loading Effect 
+
+// Skills Section Hover State - rest of icons turning black and white
 
 
 
